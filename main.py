60fb1af1918d54c5dc9294f17791e41f25b7eb1b
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-03-10 10:12:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 x,y = 2,2
-def left():
-    global x
-    if x > 0:
-        x = x-1
-def right():
+
+def vpravo():
     global x
     if x < 4:
         x = x+1
-def up():
+input.on_button_pressed(Button.B, vpravo)
+
+def vlevo():
+    global x
+    if x > 0:
+        x = x-1
+input.on_button_pressed(Button.A, vlevo)
+
+def nahoru():
     global y
     if y > 0:
         y = y-1
-def down():
+input.on_logo_event(TouchButtonEvent.PRESSED, nahoru)
+
+def dolu():
     global y
     if y < 4:
         y = y+1
+input.on_pin_pressed(TouchPin.P2, dolu)
+
 def reset():
     global x,y
     if x > 2 or x <2:
         x=2
     if y > 2 or y <2:
-                y=2
-
-input.on_button_pressed(Button.A, left)
-input.on_button_pressed(Button.B, right)
-input.on_logo_event(TouchButtonEvent.PRESSED, up)
-input.on_pin_pressed(TouchPin.P2, down)
+         y=2
 input.on_button_pressed(Button.AB, reset)
 
 def forever():
